1a292535de88368eed2452ff04935bb3ca9599d6
alifzulkifeli <alifzulkifeli@gmail.com>
2023-12-14 00:36:54 +0800

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
 import '../styles/globals.css'
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+    useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then((reg) => console.log('Service Worker registered'))
+        .catch((err) => console.error('Service Worker registration failed', err));
+    }
+  }, []);
   return <Component {...pageProps} />
 }
 
